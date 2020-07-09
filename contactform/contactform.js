a4d5c333d02a6927cd776a3c62f74e0d28061aec
Aparna <happiestme777@gmail.com>
2020-07-10 02:27:16 +0530

    clear

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -92,6 +92,7 @@ jQuery(document).ready(function($) {
     else var str = $(this).serialize();
     var action = $(this).attr('action');
     if( ! action ) {
+      alert(important);
       action = 'contactform/contactform.php';
     }
     $.ajax({
